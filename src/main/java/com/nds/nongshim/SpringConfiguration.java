8cc812cbe227c9a8d69a56624bc35ac8a78fae1c
suhwan0824 <suhwan0824@naver.com>
2022-05-13 16:17:56 +0900

    농심 인턴 프로젝트

--- src/main/java/com/nds/nongshim/SpringConfiguration.java
+++ src/main/java/com/nds/nongshim/SpringConfiguration.java
@@ -1,30 +1,38 @@
 package com.nds.nongshim;
 
-import com.nds.nongshim.repository.JDBCMemberRepository;
+import com.nds.nongshim.repository.JpaMemberRepository;
 import com.nds.nongshim.repository.MemberRepository;
-import com.nds.nongshim.repository.MemoryMemberRepository;
 import com.nds.nongshim.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfiguration {
 
-    private final DataSource dataSource;
+    /*private final DataSource dataSource;
+    private final EntityManager em;
 
-    public SpringConfiguration(DataSource dataSource) {
+    public SpringConfiguration(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
+    }*/
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfiguration(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+  /*  @Bean
     public MemberRepository memberRepository() {
-        return new JDBCMemberRepository(dataSource);
-    }
+        // return new MemoryMemberRepository();
+        // return new JDBCMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }*/
 }
---
+++ src/main/java/com/nds/nongshim/aop/TimTraceAop.java
@@ -0,0 +1,27 @@
+package com.nds.nongshim.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimTraceAop {
+
+    @Around("execution(* com.nds.nongshim..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        System.out.println("START = " + joinPoint.toString());;
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+
+            System.out.println("END = " + joinPoint.toString() + " " + timeMs + "ms");;
+        }
+    }
+}
