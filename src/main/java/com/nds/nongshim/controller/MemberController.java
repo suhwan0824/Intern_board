85660499859b61897743d90ab7709cda286d9e04
suhwan0824 <suhwan0824@naver.com>
2022-05-16 15:56:41 +0900

    농심 인턴 프로젝트

--- src/main/java/com/nds/nongshim/controller/MemberController.java
+++ src/main/java/com/nds/nongshim/controller/MemberController.java
@@ -34,13 +34,6 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @PostMapping(value = "/members/login")
-    public String login(String id, String password)
-    {
-        boolean id_same, pass_same;
-
-    }
-
     @GetMapping("/members")
     public String list(Model model){
         List<Member> members = memberService.findMembers();
