02d052b49c1157842223f55e918f240c05655da2
suhwan0824 <suhwan0824@naver.com>
2022-05-17 14:32:53 +0900

    농심 인턴 프로젝트

--- src/main/java/com/nds/nongshim/SpringConfiguration.java
+++ src/main/java/com/nds/nongshim/SpringConfiguration.java
@@ -1,38 +1,9 @@
 package com.nds.nongshim;
 
-import com.nds.nongshim.repository.JpaMemberRepository;
-import com.nds.nongshim.repository.MemberRepository;
-import com.nds.nongshim.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfiguration {
 
-    /*private final DataSource dataSource;
-    private final EntityManager em;
-
-    public SpringConfiguration(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
-    }*/
-
-    private final MemberRepository memberRepository;
-
-    public SpringConfiguration(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository);
-    }
-
-  /*  @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        // return new JDBCMemberRepository(dataSource);
-        // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }*/
 }
