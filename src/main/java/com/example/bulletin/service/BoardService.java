9600de7e6e7086fc209c7b0de9d77c0da408f444
suhwan0824 <suhwan0824@naver.com>
2022-05-18 17:26:58 +0900

    20220518

--- src/main/java/com/example/bulletin/service/BoardService.java
+++ src/main/java/com/example/bulletin/service/BoardService.java
@@ -5,13 +5,18 @@ import com.example.bulletin.dto.BoardRequestDto;
 import com.example.bulletin.dto.BoardResponseDto;
 import com.example.bulletin.entity.board.Board;
 import com.example.bulletin.entity.board.BoardRepository;
+import com.example.bulletin.json.JsonReader;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 
@@ -23,6 +28,23 @@ public class BoardService {
     @Transactional
     public Long save(BoardRequestDto boardSaveDto){
         return boardRepository.save(boardSaveDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long saveJson(BoardRequestDto boardRequestDto) throws IOException, JSONException {
+        JSONObject bigJson = JsonReader.readJsonFromUrl("https://nzin.nongshim.com/api/nz_webzine.php?rows=300&type=json");
+
+        try {
+            JSONArray next = (JSONArray) bigJson.get("array");
+            for (int i = 0; i < next.length(); i++) {
+                JSONObject smallJson = (JSONObject) next.get(i);
+
+                return Board
+                        .builder()
+                        .title(smallJson.get("title"))
+                        .registerId().build().getId();
+            }
+        }
     }
 
     @Transactional(readOnly=true)
